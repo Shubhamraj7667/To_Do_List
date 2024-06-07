177928c23d239b8904eda622eb69e45d17042857
Shubham <shubham12raj@gmail.com>
2024-06-07 14:17:46 +0530

    Added delete button and fixed some bugs

--- asset/code.php
+++ asset/code.php
@@ -23,14 +23,32 @@ if(isset($_POST["checking_edit"])){
     }
 
 
-    if(isset($_PoST["checking_delete"])){
-        $id = $_PoST["todo_id"];
-        $query = "DELETE  FROM to_do_list WHERE id = $id";
+    if(isset($_POST["checking_delete"])){
+        // print_r($_POST);
+        $id = $_POST["todo_id"];
+        $query = "DELETE FROM to_do_list WHERE id = '$id'";
         $query_run = mysqli_query($conn, $query);
 
+        
         if($query_run){
             echo $return = "Successfully Deleted";
 
         }else
         echo $return = "Something Went Wrong.!";
+    }
+
+
+    if(isset($_POST['checking_add'])){
+        $to_do = $_POST['to_do'];
+        $due_date = $_POST['due_date'];
+        $query = "INSERT INTO to_do_list (to_do,due_date,priority, `status`, updated_at, created_at ) VALUES('$to_do', '$due_date', '1', 'Active',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP )";
+        $query_run = mysqli_query($conn, $query);
+
+        if($query_run){
+            echo $return = "Successfully Stored";
+        }else{
+            echo $return = "Something Went Wrong.!";
+        }
+
+
     }
